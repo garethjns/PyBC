bc5c9bb6dae5d4ec00cfc4dbd2c8d078c3d90af2
Gareth Jones <garethjns4@gmail.com>
2018-04-15 17:12:44 +0100

    Adding hash functions

--- utils.py
+++ utils.py
@@ -7,6 +7,8 @@ Created on Fri Apr 06 17:42:17 2018
 
 #%% Imports
 
+import hashlib
+
 
 #%% Functions
 
@@ -16,3 +18,21 @@ def rev_hex(h):
     and renecoding
     """
     return h.decode("hex")[::-1].encode("hex")
+
+def hash_SHA256_twice(by):
+    """
+    Use hashlib to hash with SHA256 twice, expects binary input
+    """
+    h1 = hashlib.sha256(by).digest()
+    h2 = hashlib.new('ripemd160', h1).digest()
+    
+    return h2
+
+def hash_SHA256_ripemd160(by):
+    """
+    Use hashlib to hash with SHA256, then ripemd-160, expects binary input
+    """
+    h1 = hashlib.sha256(by).digest()
+    h2 = hashlib.new('ripemd160', h1).digest()
+    
+    return h2
