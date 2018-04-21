b4660543d3313557641f4de7b75adad76771f917
Gareth Jones <garethjns4@gmail.com>
2018-04-21 23:54:59 +0100

    Adding output address functions to methods

--- Blocks.py
+++ Blocks.py
@@ -6,7 +6,7 @@
 # %% Imports
 
 from datetime import datetime as dt
-from utils import hash_SHA256_twice, tqdm_off
+from utils import hash_SHA256_twice, tqdm_off, OP_CODES
 import mmap
 import time
 import requests
@@ -827,6 +827,49 @@ class TxOut(Common):
         """
         return self._pkScript.encode("hex")
 
+    @property
+    def parsed_pkScript(self):
+        return self.split_script()
+
+    @property
+    def outputAddr(self):
+        pass
+
+    def split_script(self):
+        pk_op = self.pkScript
+        # Create list to store output script
+        script = []
+        # Use cursor to track position in string
+        cur = 0
+        # Loop over raw script - increments 4 bytes each iteration
+        # unless instructed otherwise
+        while cur < len(pk_op):
+            # Get the next 4 bytes
+            # Convert to int in base 16
+            op = int(pk_op[cur:cur+2], 16)
+
+            # Incremenet the cursor by 4 bytes
+            cur += 2
+
+            # If the code is between 1-75, it's a number of bytes
+            # to add to stack
+            if (op >= 1) & (op <= 75):
+                # Get these and add these to script
+                script += ['PUSH_BYTES', op, pk_op[cur:cur+op * 2]]
+                cur += op * 2
+            else:
+                # Otherwise, get the OP_CODE from the dictionary
+                # If it's for an undefined code, return the code number
+                script += [OP_CODES.get(op, op)]
+
+        return script
+
+    def get_P2PKH(self):
+        pass
+
+    def get_PK2Addr(self):
+        pass
+
     def read_out(self):
         # TxOut:
         # Read value in Satoshis: 8 bytes
@@ -870,12 +913,12 @@ if __name__ == "__main__":
 
     # %% Read chain - 1 step
 
-    c = Chain(verb=2)
+    c = Chain(verb=3)
     c.read_next_Dat()
 
     # %% Read chain - all (in range)
 
-    c = Chain(verb=2,
+    c = Chain(verb=1,
               datStart=0,
               datn=3)
     c.read_all()
